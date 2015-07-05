18622d188009b137540fccf921dfd13c886a8cd7
sunatomo <tomokisunaga@yahoo.co.jp>
2015-07-06 00:43:58 +0900

    Create webmouse.js

---
+++ webmouse.js
@@ -0,0 +1,9 @@
+
+// Enable pusher logging - don't include this in production
+var pusher = new Pusher('9884e051e0816a0f6f59', {
+  encrypted: true
+});
+var channel = pusher.subscribe('test_channel');
+channel.bind('my_event', function(data) {
+  alert(data.message);
+});
