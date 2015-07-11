e4144935656eade6498f5c91c89310c8671122d4
sunatomo <tomokisunaga@yahoo.co.jp>
2015-07-11 11:50:45 +0900

    Update webmouse.js

--- webmouse.js
+++ webmouse.js
@@ -1,12 +1,4 @@
 
-var pusher = new Pusher('9884e051e0816a0f6f59', {
-  encrypted: true
-});
-var channel = pusher.subscribe('test_channel');
-channel.bind('my_event', function(data) {
-  alert(data.message);
-});
-
 window.addEventListener("load", function(){
   var iframe = document.createElement('iframe');
   iframe.src='http://sunatomo.github.io/webmouse/iframe.html';
