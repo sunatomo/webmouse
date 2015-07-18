60c8dfd678fe4b7867413d9c9333253dd1e19dff
sunatomo <tomokisunaga@yahoo.co.jp>
2015-07-18 10:43:48 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,26 @@
-console.debug('start');
-Pusher.auth_callbacks[1]({
-  auth:"a6d8fff86b226c8d0317ae5bf3dd99cbc104c5740830687c00713dbc05f13703"
-});
-
-console.debug('end');
+(function() {
+  var scripts = document.getElementsByTagName('script');
+  for(var i=0;i<scripts.length;++i) {
+    var src = scripts[i].src;
+    if(src.indexOf('auth')>0) {
+      var p = src.split('?')[1].split('&');
+      var socket_id = '';
+      var channel_name = '';
+      for(var j=0;j<p.length;++j) {
+        var kv = p[j].split('=');
+        if(kv[0]=='socket_id') socket_id=kv[1];
+        if(kv[0]=='channel_name') channel_name=kv[1];
+      }
+      
+      new jsSHA("SHA-256", "TEXT");
+      sha.setHMACKey('638ffaef0dd3ef1c90cd','TEXT');
+      sha.update(socket_id+':'+channel_name);
+      var auth={
+        auth : sha.getHMAC('HEX');
+      };
+      Pusher.auth_callbacks[1](auth);
+      
+    }
+    
+  }
+})()
